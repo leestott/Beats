ee8d90f28f3ae3e2cafe7fe868e002a5ec64c483
Lilian Kasem <liliankasem@gmail.com>
2015-11-18 14:53:57 +0000

    Added RPi LED controls

--- Beats-UWP/Beats-UWP/MainPage.xaml.cs
+++ Beats-UWP/Beats-UWP/MainPage.xaml.cs
@@ -1,30 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.Devices.Gpio;
+using Windows.UI.Core;
 
 namespace Beats_UWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private const int LED_PIN = 12;
+        private GpioPin ledPin;
+        private GpioPinValue ledPinValue = GpioPinValue.High;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Devices.DevicesLowLevelContract", 1))
+            {
+                InitGPIO();
+            }
         }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                System.Diagnostics.Debug.WriteLine("There are no GPIO controller on this device.");
+                return;
+            }
+
+            ledPin = gpio.OpenPin(LED_PIN);
+
+            // Initialize LED to the OFF state by first writing a HIGH value
+            ledPin.Write(GpioPinValue.High);
+            ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            System.Diagnostics.Debug.WriteLine("GPIO pins initialized correctly.");
+
+            ledPinValue = (ledPinValue == GpioPinValue.Low) ?
+            GpioPinValue.High : GpioPinValue.Low;
+            ledPin.Write(ledPinValue);
+        }
+
     }
 }
